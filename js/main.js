dd185be652f6dca08d161de39e27e2ece396ae9d
WragnarokA <wilmer.arango.a@gmail.com>
2023-05-07 16:38:16 +0200

    fine

--- js/main.js
+++ js/main.js
@@ -1,40 +1,81 @@
-console.log("ciao");
+const randomNumbers = [];
 
+while( randomNumbers.length < 5 ) {
 
-const randomNambers = [];
+    const numero = getRandomNumber(1, 100);
 
-while( randomNambers.length < 5 ) {
-    const numero = getRandomNamber(1, 100);
-    
-    if (!randomNambers.includes(numero)) {
-        randomNambers.push( numero );
-        
+    if( randomNumbers.includes(numero) == false ) {
+        randomNumbers.push( numero );
     }
+
+}
+
+console.log("Numeri Casuali", randomNumbers);
+
+document.getElementById("numeri").innerText = randomNumbers;
+
+setTimeout(nascondiNumeri, 2 * 1000);
+
+setTimeout(function() {
+
+    const numeriUtente = chiediNumeri();
+    console.log("Numeri utente: ", numeriUtente);
+
+    const numeriIndovinati = confrontaNumeri(randomNumbers, numeriUtente);
+
+    setInnerTextByID("messaggio", "Hai indovinato i seguenti numeri");
+    setInnerTextByID("numeri", numeriIndovinati);
+
+}, 3 * 1000);
+
+/********************** FUNZIONI DI PAGINA **************************/
+
+function nascondiNumeri() {
+    setInnerTextByID("messaggio", "Inserisci i numeri che hai visto");
+    setInnerTextByID("numeri", "");
+}
+
+function chiediNumeri() {
+    
+    const numeriUtente = [];
+
+    while(numeriUtente.length < 5) {
+
+        const numeroDaInserire = numeriUtente.length + 1;
+        const numero = parseInt( prompt(`Inserisci il ${numeroDaInserire}° numero` ) );
+
+        if( numeriUtente.includes(numero)==false && numero>0 ) {
+            numeriUtente.push(numero);
+        }
+
+    }
+
+    return numeriUtente;
     
 }
-console.log(randomNambers);
 
-document.getElementById("numeri").innerText = randomNambers;
+/********************** FUNZIONI GENERICHE **************************/
 
-setTimeout(nascondiNumeri, 3 * 1000);
-
-setTimeout(chiediNumeri, 4 * 1000);
-
-
-
-
-function getRandomNamber(min, max) {
-    return Math.floor( Math.random() * (max - min +1) + min);
+function getRandomNumber(min, max) {
+    return Math.floor( Math.random() * (max - min +1) + min );
 }
 
-
-function nascondiNumeri(){
-    document.getElementById("messaggio").innerText = "inserice i numei que hai visto!";
-    document.getElementById("numeri").innerText = "";
+function setInnerTextByID(elemId, contenuto) {
+    document.getElementById(elemId).innerText = contenuto;
 }
+
+function confrontaNumeri(randomNumbers, userNumbers) {
+
+    const indovinati = [];
     
-function chiediNumeri(){
-    let x = prompt("inserisci numeri");
-    console.log(x);
-}
+    for (let i = 0; i < randomNumbers.length; i++) {
+        const currentNumber = randomNumbers[i];
+        
+        if(userNumbers.includes(currentNumber)) {
+            indovinati.push(currentNumber);
+        }
 
+    }
+
+    return indovinati;
+}
